35bd2b041092b3f8dac2e07a15998c99e5ff6bb1
Pikser <32457085+Pikser@users.noreply.github.com>
2019-02-17 20:56:24 +0200

    create mocks

---
+++ app/mocks/index.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    id: 1,
+    name: 'Tor Molovuch',
+    time: new Date().getTime(),
+    lastMessage: 'Hello',
+    countNewMessage: 3,
+  },
+  {
+    id: 2,
+    name: 'Tor Molovuch',
+    time: new Date().getTime(),
+    lastMessage: 'Hello',
+    countNewMessage: 3,
+  },
+  {
+    id: 3,
+    name: 'Tor Molovuch',
+    time: new Date().getTime(),
+    lastMessage: 'Hello',
+    countNewMessage: 3,
+  },
+  {
+    id: 4,
+    name: 'Tor Molovuch',
+    time: new Date().getTime(),
+    lastMessage: 'Hello',
+    countNewMessage: 3,
+  },
+  {
+    id: 5,
+    name: 'Tor Molovuch',
+    time: new Date().getTime(),
+    lastMessage: 'Hello',
+    countNewMessage: 3,
+  },
+  {
+    id: 6,
+    name: 'Tor Molovuch',
+    time: new Date().getTime(),
+    lastMessage: 'Hello',
+    countNewMessage: 3,
+  },
+];
