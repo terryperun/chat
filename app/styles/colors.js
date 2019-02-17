31f3b3e333ac663b00950fd8b92c618754db6a75
Pikser <32457085+Pikser@users.noreply.github.com>
2019-02-17 21:01:23 +0200

    add constant styles  with colors

---
+++ app/styles/colors.js
@@ -0,0 +1,13 @@
+export default {
+  white: '#FFFFFF',
+  black: 'black',
+  green: 'green',
+  gray: 'gray',
+  transparent: 'transparent',
+  border: '#d6d7da',
+  background: '#f6f6f7',
+  text: '#212121',
+  grayText: '#9E9E9E',
+  icon: '#757575',
+  danger: 'red',
+};
