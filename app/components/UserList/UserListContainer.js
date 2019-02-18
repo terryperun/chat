ce1f250ff738677aa89262b349e68877b82c730c
Pikser <32457085+Pikser@users.noreply.github.com>
2019-02-18 10:17:11 +0200

    navigate to screen chat

--- app/components/UserList/UserListContainer.js
+++ app/components/UserList/UserListContainer.js
@@ -1,12 +1,17 @@
 import { compose, withHandlers } from 'recompose';
+import { withNavigation } from 'react-navigation';
 
 import UserListView from './UserListView';
 
 const enhance = compose(
+  withNavigation,
   withHandlers({
     cutMessage: (props) => (message) => {
       const shortMessage = message.substring(0, 40) + '...';
       return shortMessage;
+    },
+    onPress: (props) => () => {
+      props.navigation.navigate('Chat', {});
     },
   }),
 );
