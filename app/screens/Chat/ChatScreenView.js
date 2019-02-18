d1e8ed657d2dcb3e52c771280504a5541579742d
Pikser <32457085+Pikser@users.noreply.github.com>
2019-02-18 10:09:20 +0200

    add screens

---
+++ app/screens/Chat/ChatScreenView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ChatScreenView = () => (
+  <View>
+    <Text>CHAT</Text>
+  </View>
+);
+
+export default ChatScreenView;
