1a61cd310a201ad48b2fc615e04736e79c3cc473
Pikser <32457085+Pikser@users.noreply.github.com>
2019-03-12 10:23:35 +0200

    add animation settings for android

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { UIManager } from 'react-native';
 import RootNavigator from './app/navigation/RootNavigator';
 import store from './app/store';
 import { Provider } from 'react-redux';
+
+if (UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
 const App = () => (
   <Provider store={store}>
